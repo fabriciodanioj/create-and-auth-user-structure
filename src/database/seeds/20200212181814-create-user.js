2055884716ac8679c06212e297dc1005220f3390
fabriciodanioj <fabriciodanioj@gmail.com>
2020-02-12 15:22:39 -0300

    Inittial Commit

---
+++ src/database/seeds/20200212181814-create-user.js
@@ -0,0 +1,21 @@
+const bcrypt = require('bcryptjs');
+
+module.exports = {
+  up: QueryInterface => {
+    return QueryInterface.bulkInsert(
+      'users',
+      [
+        {
+          name: 'Fabricio Dani de Oliveira Júnior',
+          email: 'fabriciodanioj@gmail.com',
+          password_hash: bcrypt.hashSync('123456', 8),
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+
+  down: () => {},
+};
